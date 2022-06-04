18a8fc58e32340d730da7ceed987bef061615ef8
ivan.kravchenko <kravchenko19971808@gmail.com>
2022-06-05 00:06:16 +0800

    feat: lazy loading for about component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AboutComponent
   ],
   imports: [
     BrowserModule,
